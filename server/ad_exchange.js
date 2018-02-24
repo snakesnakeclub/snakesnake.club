0ba7b68838849aaac6191e6530d9d0684fcbaf86
Greg <greg.mehdiyev@mail.utoronto.ca>
2018-02-24 12:01:07 -0500

    made the exchange functions

---
+++ server/ad_exchange.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+fetch('https://api.github.com/users/github')
+    .then(res => res.price_usd)
+
+
+module.exports = {
+    function value_of_coin(total_usd_earned, total_num_coins) {
+    return total_usd_earned/total_num_coins;
+    }
+
+    function earned_from_coin(total_shares, total_xmr) {
+    const total_usd = total_xmr * price_usd
+    const usd_per_share = total_usd / total_shares
+    return total_usd;
+    }
+
+    function earned_from_ads(total_num_impr, usd_per_impr) {
+    return total_num_impr*usd_per_impr;
+    }
+
+    function total_usd_earned(earned_from_coin(),  earned_from_ads()) {
+    return earned_from_coin + earned_from_ads();
+    }
+}
