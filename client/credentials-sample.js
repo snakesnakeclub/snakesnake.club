f6689f3e39b1da893bff611fe13076ef78c4b412
Omar Chehab <omarchehab98@gmail.com>
2018-02-28 22:19:38 -0500

    Add sample credential files

---
+++ client/credentials-sample.js
@@ -0,0 +1,5 @@
+/* eslint-env browser */
+export const SOCKET_SERVER = 'http://localhost:3000';
+export const LIB_URL = 'http://localhost:3000/lib/';
+export const WEBSOCKET_SHARDS = [[SOCKET_SERVER]];
+export const REQUIRES_AUTH = false;
---
+++ server/credentials-sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+  CLIENT_URL: 'http://localhost:3000',
+  MONGO_URL: 'mongodb://localhost/snakesnake',
+  SOCKET_SERVER_PATH: '/socket.io',
+  SMTP_HOST: 'smtp.mailtrap.io',
+  SMTP_PORT: 2525,
+  SMTP_SECURE: false, // true for 465, false for other ports
+  SMTP_USER: 'your user',
+  SMTP_PASS: 'your password',
+  SMTP_FROM: 'no-reply@snakesnake.club'
+}
