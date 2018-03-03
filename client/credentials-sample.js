d298aa77e099679c8320ff9fab76f1edef8abc1a
Omar Chehab <omarchehab98@gmail.com>
2018-03-03 00:48:10 -0500

    Update credentials-sample.js

--- client/credentials-sample.js
+++ client/credentials-sample.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 export const SOCKET_SERVER = 'http://localhost:3000';
+export const SOCKET_SERVER_PATH = undefined;
 export const LIB_URL = 'http://localhost:3000/lib/';
 export const WEBSOCKET_SHARDS = [[SOCKET_SERVER]];
 export const REQUIRES_AUTH = false;
