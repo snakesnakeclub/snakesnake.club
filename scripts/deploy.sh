67e15dc6eacbcd6a59a2e4aa09c9dfac9c542db8
Brennan Gebotys <gebob19@gmail.com>
2018-03-03 21:42:01 -0500

    Add ssh-add to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@ eval $(ssh-agent)
 mkdir ~/.ssh
 echo "${DEPLOY_KEY}" | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 ssh -o 'StrictHostKeyChecking=no' \
   "travis@${SSH_HOST}" \
   'make -C snakesnake.club'
