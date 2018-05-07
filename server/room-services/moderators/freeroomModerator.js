c89f1a7ca863fb478e683efe9d220898c360b618
Brennan Gebotys <gebob19.git@gmail.com>
2018-05-06 21:00:07 -0400

    fixed valid position bug

--- server/room-services/moderators/freeroomModerator.js
+++ server/room-services/moderators/freeroomModerator.js
@@ -181,7 +181,7 @@ module.exports = class FreeRoomModerator extends Moderator {
     var valid = false;
     do {
       player.reset();
-      var alivePlayers = Array.from(this.alivePlayers);
+      var alivePlayers = Array.from(this.alivePlayers.keys());
       valid = !alivePlayers.some(alivePlayer => {
         return alivePlayer.pieces.some(piece => {
           return piece.isCollidingWith(player.pieces[0]);
